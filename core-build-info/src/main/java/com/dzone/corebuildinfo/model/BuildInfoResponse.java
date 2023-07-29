b62f4afff74e7240e54c25d04d7e0d5ee09fe041
Amol Gote <aamolgote@hotmail.com>
2023-07-29 08:41:50 -0400

    Build info - other attributes

--- core-build-info/src/main/java/com/dzone/corebuildinfo/model/BuildInfoResponse.java
+++ core-build-info/src/main/java/com/dzone/corebuildinfo/model/BuildInfoResponse.java
@@ -1,9 +1,15 @@
 package com.dzone.corebuildinfo.model;
 
+import java.time.Instant;
+
 public class BuildInfoResponse {
     private String name;
     private String version;
-    private String time;
+    private Instant time;
+    private String artifact;
+    private String group;
+    private String[] activeProfile;
+    private String springVersion;
 
     public String getName() {
         return name;
@@ -21,11 +27,43 @@ public class BuildInfoResponse {
         this.version = version;
     }
 
-    public String getTime() {
+    public Instant getTime() {
         return time;
     }
 
-    public void setTime(String time) {
+    public void setTime(Instant time) {
         this.time = time;
+    }
+
+    public String getArtifact() {
+        return artifact;
+    }
+
+    public void setArtifact(String artifact) {
+        this.artifact = artifact;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String[] getActiveProfile() {
+        return activeProfile;
+    }
+
+    public void setActiveProfile(String[] activeProfile) {
+        this.activeProfile = activeProfile;
+    }
+
+    public String getSpringVersion() {
+        return springVersion;
+    }
+
+    public void setSpringVersion(String springVersion) {
+        this.springVersion = springVersion;
     }
 }
--- core-build-info/src/main/java/com/dzone/corebuildinfo/service/BuildInfoService.java
+++ core-build-info/src/main/java/com/dzone/corebuildinfo/service/BuildInfoService.java
@@ -3,6 +3,7 @@ package com.dzone.corebuildinfo.service;
 import com.dzone.corebuildinfo.model.BuildInfoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,18 @@ public class BuildInfoService {
     @Autowired
     BuildProperties buildProperties;
 
+    @Autowired
+    Environment environment;
+
     public BuildInfoResponse getBuildInfo(){
         BuildInfoResponse buildInfoResponse = new BuildInfoResponse();
         buildInfoResponse.setName(this.buildProperties.getName());
         buildInfoResponse.setVersion(this.buildProperties.getVersion());
+        buildInfoResponse.setTime(this.buildProperties.getTime());
+        buildInfoResponse.setActiveProfile(this.environment.getActiveProfiles());
+        buildInfoResponse.setSpringVersion(this.buildProperties.getVersion());
+        buildInfoResponse.setGroup(this.buildProperties.getGroup());
+        buildInfoResponse.setArtifact(this.buildProperties.getArtifact());
         return buildInfoResponse;
     }
 }
